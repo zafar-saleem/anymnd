4a6fd8035e0d94794ac0e14604049aad20c6e6bb
Zafar Saleem <zafarsaleem3@gmail.com>
2021-09-15 19:43:15 +0500

    Added grid and padding to title

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,15 +1,28 @@
 import styled from 'styled-components';
 import Title from 'components/title/';
 import Sidebar from 'pages/chat/components/sidebar/';
+import ChatContents from 'pages/chat/components/chatContents/';
 
 const Wrapper = styled.div`
 	padding: 20px;
-	// background: red;
 	max-width: 1300px;
+	height: 100%;
 	margin: auto;
-	@media (max=max-width: 450px) {
-		overflow: hidden;
+	@media (max-width: 450px) {
+		padding: 0;
+		// overflow: hidden;
+
+		& > .title {
+			padding: 30px 0;
+		}
 	}
+`;
+
+const Grid = styled.div`
+	display: grid;
+	height: 100%;
+	grid-template-columns: 1fr 2fr;
+	background-color: #f4f5fb;
 `;
 
 const Chat = () => {
@@ -19,7 +32,10 @@ const Chat = () => {
 				title='1 day chat App'
 				notice='All messages will be deleted at every 00:00 UTC'
 			/>
-			<Sidebar />
+			<Grid>
+				<Sidebar />
+				<ChatContents />
+			</Grid>
 		</Wrapper>
 	);
 }
