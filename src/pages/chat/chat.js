5bc782577799c2d101de5034287d0302d13bc7bb
Zafar Saleem <zafarsaleem3@gmail.com>
2021-09-15 15:43:12 +0500

    Added child components into chat

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,8 +1,24 @@
 import styled from 'styled-components';
+import Title from 'components/title/';
+import Sidebar from 'pages/chat/components/sidebar/';
+
+const Wrapper = styled.div`
+	padding: 20px;
+	// background: red;
+	@media (max=max-width: 450px) {
+		overflow: hidden;
+	}
+`;
 
 const Chat = () => {
 	return (
-		<div></div>
+		<Wrapper>
+			<Title
+				title='1 day chat App'
+				notice='All messages will be deleted at every 00:00 UTC'
+			/>
+			<Sidebar />
+		</Wrapper>
 	);
 }
 
